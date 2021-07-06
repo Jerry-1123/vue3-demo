087277ca759325365903286033fef4d4fbd856dd
夏润豪 <610500714@qq.com>
2021-07-06 17:15:27 +0800

    x

---
+++ src/views/Test.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>{{ count }} * 2 = {{ double }}</h1>
+  <button @click="handleAdd">累加</button>
+  <h1>{{ title }}</h1>
+  <button @click="handleClickTitle">👋</button>
+</template>
+
+<script>
+import { reactive, computed, toRefs, onMounted, onUnmounted } from 'vue'
+
+export default {
+  name: 'Test',
+  setup () {
+
+    let state = reactive({
+      count: 1,
+      title: 'hello world'
+    })
+
+    const double = computed(() => state.count * 2)
+
+    const handleAdd = () => state.count++
+
+    const handleClickTitle = () => state.title = state.title.toUpperCase()
+
+    onMounted(() => {
+      console.log('onMounted')
+    })
+
+    onUnmounted(() => {
+      console.log('onUnmounted')
+    })
+
+    return {
+      ...toRefs(state),
+      double,
+      handleAdd,
+      handleClickTitle
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
